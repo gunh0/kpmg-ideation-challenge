9cec5b05e08eca496cd3620cbd050dc5df84fb86
devgunho <parkgh3737@naver.com>
2020-02-10 01:06:08 +0900

    commit

--- basic_crawling.py
+++ basic_crawling.py
@@ -1,8 +1,24 @@
-import urllib.request
-from bs4 import BeautifulSoup as bs
+#import urllib.request
+from urllib.parse import quote_plus
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementNotVisibleException
 
-url = 'https://patents.google.com/?q=test&oq=test'
-html = urllib.request.urlopen(url).read()
-soup = bs(html, 'html.parser')
+import pyperclip
 
-print(soup)
+baseurl = 'https://patents.google.com/?q='
+plusurl = input('Search : ')
+url = baseurl + quote_plus(plusurl)
+
+driver = webdriver.Chrome()
+driver.get(url)
+
+try:
+    element = WebDriverWait(driver, 10).until(
+        expected_conditions.presence_of_element_located((By.ID, "count"))
+    )
+finally:
+    driver.find_element_by_css_selector('#count > div > span > a').click()
+    #driver.quit()
