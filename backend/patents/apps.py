e84b496ac77060d9c8741dc558d8357f02252b6c
gunh0 <gunho0902@outlook.kr>
2022-02-28 21:30:07 +0900

    feat: add Dataset and Patent models

---
+++ backend/patents/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PatentsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "patents"
---
+++ backend/patents/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+
+
+class Dataset(models.Model):
+    """One Google Patents search result export."""
+
+    name = models.CharField(max_length=200)
+    search_url = models.URLField(max_length=2000, blank=True)
+    imported_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-imported_at"]
+
+    def __str__(self):
+        return self.name
+
+
+class Patent(models.Model):
+    """A patent or application as listed in a Google Patents export."""
+
+    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE, related_name="patents")
+    patent_id = models.CharField(max_length=64)
+    title = models.TextField()
+    assignee = models.CharField(max_length=500, blank=True)
+    inventors = models.TextField(blank=True)
+    priority_date = models.DateField(null=True, blank=True)
+    filing_date = models.DateField(null=True, blank=True)
+    publication_date = models.DateField(null=True, blank=True)
+    grant_date = models.DateField(null=True, blank=True)
+    result_link = models.URLField(max_length=500, blank=True)
+    figure_link = models.URLField(max_length=500, blank=True)
+
+    class Meta:
+        ordering = ["-publication_date", "patent_id"]
+        constraints = [
+            models.UniqueConstraint(fields=["dataset", "patent_id"], name="unique_patent_per_dataset"),
+        ]
+
+    def __str__(self):
+        return f"{self.patent_id} {self.title}"
+
+    @property
+    def is_granted(self):
+        return self.grant_date is not None
+
+    @property
+    def inventor_list(self):
+        return [name.strip() for name in self.inventors.split(",") if name.strip()]
