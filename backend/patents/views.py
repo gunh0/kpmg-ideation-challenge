4e6c9f686598ca228005fe2c959c9d754202c1fc
gunh0 <gunho0902@outlook.kr>
2022-03-09 21:54:19 +0900

    feat: paginate and order the patent list

--- backend/patents/views.py
+++ backend/patents/views.py
@@ -3,12 +3,16 @@ from rest_framework import filters, viewsets
 
 from .filters import PatentFilter
 from .models import Patent
+from .pagination import PatentPagination
 from .serializers import PatentSerializer
 
 
 class PatentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Patent.objects.select_related("dataset")
     serializer_class = PatentSerializer
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    pagination_class = PatentPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_class = PatentFilter
     search_fields = ["patent_id", "title", "assignee", "inventors"]
+    ordering_fields = ["publication_date", "priority_date", "filing_date", "grant_date", "patent_id", "title"]
+    ordering = ["-publication_date", "patent_id"]
