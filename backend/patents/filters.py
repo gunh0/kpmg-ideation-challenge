c0ae07031562adf46adc1ebb1d508d74a6c46e8f
gunh0 <gunho0902@outlook.kr>
2022-03-10 20:12:02 +0900

    fix: keep patents without the sorted date at the end of the list

--- backend/patents/filters.py
+++ backend/patents/filters.py
@@ -1,4 +1,6 @@
 import django_filters
+from django.db.models import F
+from rest_framework.filters import OrderingFilter
 
 from .models import Patent
 
@@ -12,3 +14,20 @@ class PatentFilter(django_filters.FilterSet):
     class Meta:
         model = Patent
         fields = ["dataset", "assignee", "granted", "year_from", "year_to"]
+
+
+class NullsLastOrderingFilter(OrderingFilter):
+    """Ordering that keeps patents without the sorted date at the end,
+    whichever direction is requested."""
+
+    def filter_queryset(self, request, queryset, view):
+        ordering = self.get_ordering(request, queryset, view)
+        if not ordering:
+            return queryset
+        expressions = []
+        for field in ordering:
+            if field.startswith("-"):
+                expressions.append(F(field[1:]).desc(nulls_last=True))
+            else:
+                expressions.append(F(field).asc(nulls_last=True))
+        return queryset.order_by(*expressions)
--- backend/patents/views.py
+++ backend/patents/views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 
-from .filters import PatentFilter
+from .filters import NullsLastOrderingFilter, PatentFilter
 from .models import Patent
 from .pagination import PatentPagination
 from .serializers import PatentSerializer
@@ -11,7 +11,7 @@ class PatentViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Patent.objects.select_related("dataset")
     serializer_class = PatentSerializer
     pagination_class = PatentPagination
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, NullsLastOrderingFilter]
     filterset_class = PatentFilter
     search_fields = ["patent_id", "title", "assignee", "inventors"]
     ordering_fields = ["publication_date", "priority_date", "filing_date", "grant_date", "patent_id", "title"]
