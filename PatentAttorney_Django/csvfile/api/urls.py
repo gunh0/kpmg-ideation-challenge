21cae749453f5d322c2df5557dca42c25a186e40
gunh0 <gunho0902@outlook.kr>
2022-03-22 21:28:45 +0900

    refactor: replace the legacy Django project with the new backend

--- PatentAttorney_Django/csvfile/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import PatentListView, PatentDetailView
-
-urlpatterns = [
-    path('',PatentListView.as_view()),
-    path('<pk>',PatentDetailView.as_view())
-]
--- PatentAttorney_Django/csvfile/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CsvfileConfig(AppConfig):
-    name = 'csvfile'
