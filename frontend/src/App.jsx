fda63bab1f816b7fd6360d9434443061b647fe13
gunh0 <gunho0902@outlook.kr>
2022-03-31 21:44:48 +0900

    feat: scaffold React 18 frontend with Vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="app">
+      <h1>Patent Attorney Without Borders</h1>
+      <p>Explore Google Patents search results by assignee, inventor and year.</p>
+    </main>
+  );
+}
---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// In development the API runs on :8000; proxying /api keeps requests same-origin.
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": process.env.API_URL || "http://localhost:8000",
+    },
+  },
+});
