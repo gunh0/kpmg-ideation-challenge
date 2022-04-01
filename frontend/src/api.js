45d82d9e454fa70d9dc85a75d45f0259e71109db
gunh0 <gunho0902@outlook.kr>
2022-04-01 20:44:45 +0900

    feat: add a client for the backend API

---
+++ frontend/src/api.js
@@ -0,0 +1,55 @@
+// Thin client for the Django API. In development Vite proxies /api to :8000,
+// in Docker nginx does, so relative URLs work everywhere.
+
+export class ApiError extends Error {
+  constructor(message, status, body) {
+    super(message);
+    this.status = status;
+    this.body = body;
+  }
+}
+
+export function toQuery(params = {}) {
+  const query = new URLSearchParams();
+  Object.entries(params).forEach(([key, value]) => {
+    if (value !== undefined && value !== null && value !== "") query.set(key, value);
+  });
+  const text = query.toString();
+  return text ? `?${text}` : "";
+}
+
+async function request(path, options = {}) {
+  const response = await fetch(`/api/${path}`, {
+    headers: { Accept: "application/json" },
+    ...options,
+  });
+  if (response.status === 204) return null;
+  const body = await response.json().catch(() => null);
+  if (!response.ok) {
+    throw new ApiError(errorMessage(body) || `Request failed (${response.status})`, response.status, body);
+  }
+  return body;
+}
+
+// DRF errors look like {"detail": "..."} or {"field": ["..."]}.
+export function errorMessage(body) {
+  if (!body || typeof body !== "object") return "";
+  if (typeof body.detail === "string") return body.detail;
+  return Object.values(body).flat().filter((value) => typeof value === "string").join(" ");
+}
+
+export const api = {
+  datasets: () => request("datasets/"),
+  uploadDataset: (file, name = "") => {
+    const form = new FormData();
+    form.append("file", file);
+    if (name) form.append("name", name);
+    return request("datasets/", { method: "POST", body: form });
+  },
+  deleteDataset: (id) => request(`datasets/${id}/`, { method: "DELETE" }),
+  patents: (params) => request(`patents/${toQuery(params)}`),
+  patent: (id) => request(`patents/${id}/`),
+  stats: (params) => request(`stats/${toQuery(params)}`),
+  assignees: (params) => request(`assignees/${toQuery(params)}`),
+  exportUrl: (params) => `/api/patents/export/${toQuery(params)}`,
+};
