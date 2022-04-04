d7515e5a87d2bf5938c7cef2634199ef70ef5517
gunh0 <gunho0902@outlook.kr>
2022-04-04 20:00:33 +0900

    feat: search patents as you type

---
+++ frontend/src/hooks/useDebounce.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+
+export default function useDebounce(value, delay = 300) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebounced(value), delay);
+    return () => clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounced;
+}
--- frontend/src/pages/Patents.jsx
+++ frontend/src/pages/Patents.jsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
 import { api } from "../api";
 import PatentTable from "../components/PatentTable";
 import useApi from "../hooks/useApi";
+import useDebounce from "../hooks/useDebounce";
 
 export default function Patents() {
-  const { data, error, loading } = useApi(() => api.patents(), []);
+  const [search, setSearch] = useState("");
+  const query = useDebounce(search.trim());
+  const { data, error, loading } = useApi(() => api.patents({ search: query }), [query]);
 
   return (
     <section>
       <h1 className="page-title">Patents</h1>
       <p className="page-lead">Search and filter the patents of the selected dataset.</p>
+
+      <div className="toolbar">
+        <input
+          type="search"
+          className="input search"
+          placeholder="Search id, title, assignee or inventor"
+          aria-label="Search patents"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+      </div>
+
       {error && <p className="error">{error.message}</p>}
       {loading && !data && <p className="muted">Loading…</p>}
       {data && (
         <>
-          <p className="muted">{data.count} patents</p>
+          <p className="muted">
+            {data.count} patents{query && <> matching “{query}”</>}
+          </p>
           <PatentTable patents={data.results} />
         </>
       )}
