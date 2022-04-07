fc8148a31e0b7c3c9626471e140713ed2b398ca7
gunh0 <gunho0902@outlook.kr>
2022-04-07 21:55:04 +0900

    feat: filter patents by grant status and publication year

--- frontend/src/pages/Patents.jsx
+++ frontend/src/pages/Patents.jsx
@@ -12,11 +12,32 @@ export default function Patents() {
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
   const [ordering, setOrdering] = useState("-publication_date");
+  const [granted, setGranted] = useState("");
+  const [yearFrom, setYearFrom] = useState("");
+  const [yearTo, setYearTo] = useState("");
   const query = useDebounce(search.trim());
+  const debouncedYearFrom = useDebounce(yearFrom);
+  const debouncedYearTo = useDebounce(yearTo);
   const { data, error, loading } = useApi(
-    () => api.patents({ search: query, ordering, page, page_size: PAGE_SIZE }),
-    [query, ordering, page]
+    () =>
+      api.patents({
+        search: query,
+        granted,
+        year_from: debouncedYearFrom,
+        year_to: debouncedYearTo,
+        ordering,
+        page,
+        page_size: PAGE_SIZE,
+      }),
+    [query, granted, debouncedYearFrom, debouncedYearTo, ordering, page]
   );
+
+  function withFirstPage(setter) {
+    return (value) => {
+      setter(value);
+      setPage(1);
+    };
+  }
 
   function changeOrdering(value) {
     setOrdering(value);
@@ -42,6 +63,39 @@ export default function Patents() {
           value={search}
           onChange={(event) => changeSearch(event.target.value)}
         />
+        <select
+          className="select"
+          aria-label="Grant status"
+          value={granted}
+          onChange={(event) => withFirstPage(setGranted)(event.target.value)}
+        >
+          <option value="">All statuses</option>
+          <option value="true">Granted</option>
+          <option value="false">Applications</option>
+        </select>
+        <div className="year-range">
+          <input
+            type="number"
+            className="input year"
+            placeholder="From"
+            aria-label="Published from year"
+            min="1900"
+            max="2100"
+            value={yearFrom}
+            onChange={(event) => withFirstPage(setYearFrom)(event.target.value)}
+          />
+          <span className="muted">–</span>
+          <input
+            type="number"
+            className="input year"
+            placeholder="To"
+            aria-label="Published to year"
+            min="1900"
+            max="2100"
+            value={yearTo}
+            onChange={(event) => withFirstPage(setYearTo)(event.target.value)}
+          />
+        </div>
       </div>
 
       {error && <p className="error">{error.message}</p>}
