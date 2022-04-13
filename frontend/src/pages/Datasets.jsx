393d803d64fd42120f5ceba78c82938fc7be288e
gunh0 <gunho0902@outlook.kr>
2022-04-13 21:27:48 +0900

    feat: list imported datasets

--- frontend/src/pages/Datasets.jsx
+++ frontend/src/pages/Datasets.jsx
@@ -1,8 +1,55 @@
+import { useState } from "react";
+
+import { api } from "../api";
+import useApi from "../hooks/useApi";
+
+function formatDateTime(value) {
+  return new Date(value).toLocaleString(undefined, { dateStyle: "medium", timeStyle: "short" });
+}
+
 export default function Datasets() {
+  const [version, setVersion] = useState(0);
+  const { data, error, loading } = useApi(() => api.datasets(), [version]);
+
   return (
     <section>
       <h1 className="page-title">Datasets</h1>
       <p className="page-lead">Google Patents exports imported so far.</p>
+
+      {error && <p className="error">{error.message}</p>}
+      {loading && !data && <p className="muted">Loading…</p>}
+      {data && (
+        <div className="table-wrap">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Patents</th>
+                <th>Imported</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((dataset) => (
+                <tr key={dataset.id}>
+                  <td>
+                    <strong>{dataset.name}</strong>
+                    {dataset.search_url && (
+                      <div className="muted small ellipsis" title={dataset.search_url}>
+                        {dataset.search_url}
+                      </div>
+                    )}
+                  </td>
+                  <td>{dataset.patent_count}</td>
+                  <td className="nowrap">{formatDateTime(dataset.imported_at)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      <button type="button" className="button refresh" onClick={() => setVersion((v) => v + 1)}>
+        Refresh
+      </button>
     </section>
   );
 }
